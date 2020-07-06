394fe1bd672b01ee45c4f3446f83f6fff8b5c3e5
María Zepeda <marielos.zepeda14@gmail.com>
2020-07-05 20:29:45 -0600

    Corto número dos

---
+++ src/main/java/com/mycompany/za19006/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package com.mycompany.za19006.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/mycompany/za19006/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package com.mycompany.za19006.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
